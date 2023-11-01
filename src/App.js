da737477800e048fcf2ccf677907dbb6a81e5274
badrahaji <bguirbadreddine@gmail.com>
2023-11-01 19:42:00 +0100

    react-state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// importing our react class component 
+import badr from './badr.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//photo import
+import './style.css'
+class App extends React.Component {
+state = {  
+  time : 0,
+  show : false ,
+    Person :
+    
+    
+    {fullName : "Bguir Badreddine ",
+     bio :"full Stack Developer",
+     src: badr ,
+     profession:"batal" }
+    };
+
+    // our component life cycle manipulation
+     componentDidMount() {
+      // using interval to repeat the increment to count timer
+        setInterval(() =>{
+          this.setState({time:this.state.time +1 })
+      }, 1000);
+       
+    }
+    // changing our show value function 
+     HandleClick=({state}) => {
+
+       this.state.show===false ? this.setState({show:true}):this.setState({show:false})
+      }
+
+      render()
+  {
+    return (
+      <div style={{textAlign:"center"}}>
+    
+      <h1 id='tilte'>Am her  </h1>
+      <button onClick={this.HandleClick} > show me </button>
+       <h3 className='timer'>time spend :   {this.state.time} sec</h3>
+        <div className="content">
+       
+       {/* our show condition if true button will show content 
+       else content  will desapear*/}
+        {
+   this.state.show && (
+     <div>
+     <p><h1>{this.state.Person.fullName}</h1></p>
+        <p>{this.state.Person.bio}</p>
+        <p>{this.state.Person.profession}</p>
+        <img src={this.state.Person.src} alt='myphoto' />
+        </div>
+      
+    )}
+      </div>
     </div>
-  );
+    )   
 }
-
+}
 export default App;
